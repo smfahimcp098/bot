2a6ce26689173db5f333aa4d89770d31ecb87974
S M Fahim <158431295+smfahimcp@users.noreply.github.com>
2024-12-24 23:29:14 +0600

    Create fluxanime.js

---
+++ scripts/cmds/fluxanime.js
@@ -0,0 +1,66 @@
+module.exports = {
+  config: {
+    name: 'fluxanime',
+    aliases: ['fluxa','fanime'],
+    version: '1.0',
+    author: 'Team Calyx',
+    countDown: 10,
+    role: 0,
+    longDescription: {
+      en: 'Generate an image from text using flux anime.'
+    },
+    category: 'image',
+    guide: {
+      en: '{pn} prompt [--ar=<ratio>] or [==ar <ratio>]'
+    }
+  },
+
+  onStart: async function ({ message, api, args, event }) {
+    const promptText = args.join(' ');
+
+    if (!promptText) {
+      return message.reply(`Please enter a text prompt\nExample: \n${global.GoatBot.config.prefix}fluxanime a car or,\n${global.GoatBot.config.prefix}fluxanime a girl --ar 2:3`);
+    }
+
+    let ratio = '1:1';
+
+    const ratioIndex = args.findIndex(arg => arg.startsWith('--ar='));
+    if (ratioIndex !== -1) {
+      ratio = args[ratioIndex].split('=')[1];
+      args.splice(ratioIndex, 1);
+    } else {
+      const ratioFlagIndex = args.findIndex(arg => arg === '--ar');
+      if (ratioFlagIndex !== -1 && args[ratioFlagIndex + 1]) {
+        ratio = args[ratioFlagIndex + 1];
+        args.splice(ratioFlagIndex, 2); 
+      }
+    }
+
+    api.setMessageReaction("⏳", event.messageID, () => {}, true);
+
+    const startTime = new Date().getTime();
+
+    try {
+      const prompt = args.join(' ');
+      const world = `&ratio=${ratio}`;
+      const team = `fluxanime?prompt=${encodeURIComponent(prompt)}${world}`;
+	  const o = "xyz";
+      const imageURL = `https://smfahim.${o}/${team}`;
+      const attachment = await global.utils.getStreamFromURL(imageURL);
+
+      const endTime = new Date().getTime();
+      const timeTaken = (endTime - startTime) / 1000;
+
+      message.reply({
+        body: `Here is your Flux Anime Model 🖼\nTime taken: ${timeTaken} seconds`,
+        attachment: attachment
+      });
+
+      api.setMessageReaction("✅", event.messageID, () => {}, true);
+
+    } catch (error) {
+      console.error(error);
+      api.setMessageReaction("❌", event.messageID, () => {}, true);
+    }
+  }
+};
